6cc73926d84310b719a4b26774763ab98986a0e5
pius ssekweyama <ssekweyamapius@gmail.com>
2022-11-10 13:06:23 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -23,37 +23,6 @@ const toDo = [
   },
 ];
 
-// eslint-disable-next-line no-unused-vars
-const template = `
-<li class="todo">
-  <article>
-      <input class="toggle" type="checkbox" value="" tabindex="0">
-
-      <div class="view">
-          <label class="label" tabindex="0">Double-tap to edit</label>
-          <textarea class="edit chromeless-input"  maxlength="255">Double-tap to edit</textarea>
-      </div>
-
-      <svg class="destroy" version="1.1" baseProfile="tiny" id="Layer_1" xmlns:x="&ns_extend;" xmlns:i="&ns_ai;" xmlns:graph="&ns_graphs;"
-          xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:a="http://ns.adobe.com/AdobeSVGViewerExtensions/3.0/"
-              x="0px" y="0px" width="42px" height="42px" viewBox="-0.5 0.5 42 42" xml:space="preserve">
-          <path d="M12.5,16.5v17h3v-17H12.5z M18.5,16.5v17h3v-17H18.5z M24.5,16.5v17h3v-17H24.5z M27.5,4.5c0-2.5-0.609-3-3-3h-10
-              c-2.52,0-2.98,0.55-2.98,3.01L11.5,7.5h-8c-1.48,0-2,0.49-2,2v1c0,1.55,0.52,2,2,2h1v26c0,2.49,0.55,3,3,3h24c2.5,0,4-0.471,4-3v-26
-              h1c1.51,0,2-0.48,2-2v-1c0-1.48-0.43-2-2-2h-9V4.5z M24.5,4.5v3h-10v-3H24.5z M9.5,12.5h21v24h-21V12.5z"/>
-      </svg>
-
-      <svg class="move" width="24px" height="24px" viewBox="0 0 24 24" id="three-dots" xmlns="http://www.w3.org/2000/svg">
-          <g id="_20x20_three-dots--grey" data-name="20x20/three-dots--grey" transform="translate(24) rotate(90)">
-            <rect id="Rectangle" width="24" height="24" fill="none"/>
-            <circle id="Oval" cx="1" cy="1" r="1" transform="translate(5 11)" stroke="#000" stroke-miterlimit="10" stroke-width="0.5"/>
-            <circle id="Oval-2" data-name="Oval" cx="1" cy="1" r="1" transform="translate(11 11)" stroke="#000" stroke-miterlimit="10" stroke-width="0.5"/>
-            <circle id="Oval-3" data-name="Oval" cx="1" cy="1" r="1" transform="translate(17 11)" stroke="#000" stroke-miterlimit="10" stroke-width="0.5"/>
-          </g>
-      </svg>
-
-  </article>
-</li>`;
-
 function createMove() {
   const span = document.createElement('span');
   span.classList.add('move');
