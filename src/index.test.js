801523648a5a89d15e980ec471422a2f4c2c7c57
pius ssekweyama <ssekweyamapius@gmail.com>
2022-11-18 07:49:52 +0300

    Update index.test.js

--- src/index.test.js
+++ src/index.test.js
@@ -1,16 +1,28 @@
+import { before } from 'lodash';
 import addTasks from './addTasks.js';
 import clearStorage from './clearStorage.js';
+const { localStorage, window } = require('./dom.js');
+
+jest.mock('./clearStorage.js', () => jest.fn());
 
 describe('unit tests for the To Do list application', () => {
   beforeEach(() => {
-    clearStorage();
     jest.resetAllMocks();
-  });
-  afterEach(() => {
-    console.log(localStorage.getAll());
+    clearStorage();
   });
 
   test('data is added into storage', () => {
-    addTasks();
+    clearStorage.mockImplementation(()=>{
+      localStorage.clear();
+    });
+    const mockJson = {
+      index: Object.keys(localStorage.getAll()).length + 1,
+      completed: false,
+      description: "TodoList one",
+    };
+    localStorage.setItem('todo', JSON.stringify(mockJson));
+    console.log(localStorage.getAll());
+    // addTasks();
+    //expect(clearStorage(10)).toBe(10);
   });
 });
